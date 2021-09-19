102c30f67ec559f964d5f63cc8fcd457a46c0123
Saurav <saurav.maharjan.lagcc.cuny@gmail.com>
2021-09-19 14:57:00 -0400

    unit 2

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,18 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Saurav Maharjan on 9/19/21.
+//
+
+import UIKit
+import AlamofireImage
+
+
+class MovieGridCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+}
